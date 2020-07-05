26c1e35a4cc6c0a398ef82f0e8ae7625ea15db17
Aritro Paul <aritro.paul61@gmail.com>
2020-07-06 02:08:53 +0530

    Initial Commit

---
+++ PopcornTime-Mac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopcornTime-Mac
+//
+//  Created by Aritro Paul on 06/07/20.
+//  Copyright © 2020 Aritro Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
